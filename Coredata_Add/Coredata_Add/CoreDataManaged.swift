b3ebf0d80a9704a1a1e969485ac84237e985ef3a
Đỗ Việt <dohoangviet.fpt@gmail.com>
2017-03-08 06:37:35 +0700

    insert, remove

---
+++ Coredata_Add/Coredata_Add/CoreDataManaged.swift
@@ -0,0 +1,44 @@
+//
+//  CoreDataManaged.swift
+//  Coredata_Add
+//
+//  Created by Đỗ Việt on 3/7/17.
+//  Copyright © 2017 Do Viet. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataManaged: NSObject {
+    
+    private class func getContext() -> NSManagedObjectContext {
+        
+        let appDel = UIApplication.shared.delegate as! AppDelegate
+        return appDel.persistentContainer.viewContext
+        
+    }
+    
+    class func saveObj() {
+        
+        let context = getContext()
+        
+        let entity = NSEntityDescription.entity(forEntityName: "Person", in: context)
+        
+        let managedObj = NSManagedObject(entity: entity!, insertInto: context)
+        
+        managedObj.setValue("Nguyen Van An", forKey: "fullname")
+        managedObj.setValue("an@gmail.com", forKey: "email")
+        managedObj.setValue("annv", forKey: "username")
+        managedObj.setValue("1234", forKey: "password")
+        
+        do {
+            try context.save()
+            print("saved")
+        } catch let error {
+            fatalError("Error \(error)")
+        }
+        
+    }
+    
+    
+}
